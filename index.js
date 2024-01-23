976e43d026989026e97af92e0c9069ed768b2125
Razdor13 <oneamaday@gmail.com>
2024-01-23 23:10:46 +0200

    done

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 const btn = document.querySelectorAll('.box')
 
 const evenBtn = []
-const notEvenBtn = []
+const oddBtn = []
 
-function getEvenBtn(){
+function getSortBtn(){
     btn.forEach((item,i)=>{
         if((i+1) % 2 === 0) {
             evenBtn.push(item)
             
         }else{
-            notEvenBtn.push(item)
+            oddBtn.push(item)
             
         }
     })
 }
-getEvenBtn()
-console.log(notEvenBtn, 'not')
-console.log(evenBtn)
+getSortBtn()
+
+
+const evenBtnPromise = evenBtn.map(btn => {
+    return new Promise(resolve => {
+        btn.onclick = () => resolve(btn.classList.add('active'))
+    })
+})
+const OddBtnPromise = oddBtn.map(btn => {
+    return new Promise(resolve => {
+        btn.onclick = () => resolve(btn.classList.add('active'))
+    })
+})
+
+const allPromiseEvenBtn = Promise.all(evenBtnPromise)
+.then(()=>{
+    alert('even digits are all active')
+})
+
+const allPromiseOddBtn = Promise.all(OddBtnPromise)
+.then(()=>{
+    alert('odd digits are all active')
+})
+
+
+Promise.all([allPromiseEvenBtn,allPromiseOddBtn])
+    .then(()=>{
+        alert('all digits are active')
+    }) 
